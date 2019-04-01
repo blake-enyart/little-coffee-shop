6d6b18ec3253eec4eb6d172103a50e0e0c203811
Matt Levy <milevy1@gmail.com>
2019-04-01 16:50:12 -0600

    Added validation tests for User model
    Co-authored-by: Noah Flint <34421236+n-flint@users.noreply.github.com>
    Co-authored-by: Rene Casco <34440390+renecasco@users.noreply.github.com>
    Co-authored-by: Michael Karnes <44683008+mikekarnes123@users.noreply.github.com>
    Co-authored-by: Blake Enyart <38588295+blake-enyart@users.noreply.github.com>

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+    it { should validate_presence_of :username }
+    it { should validate_presence_of :street }
+    it { should validate_presence_of :city }
+    it { should validate_presence_of :state }
+    it { should validate_presence_of :zipcode }
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :password }
+    it { should validate_presence_of :role }
+    it { should validate_presence_of :enabled }
+
+    it { should validate_uniqueness_of :email }
+  end
+
+  # describe 'Relationships' do
+  #   it { should have_many :orders } # for customers
+  #   it { should have_many :items } # for merchants
+  # end
+end
