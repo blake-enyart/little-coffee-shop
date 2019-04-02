69404c7625c14546c2b03f7bb184785f85d4c385
Matt Levy <milevy1@gmail.com>
2019-04-01 21:10:16 -0600

    Added tests for Order model

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
+  describe 'Validations' do
+    it { should validate_presence_of :status }
+  end
 
+  describe 'Relationships' do
+    it { should belong_to :user }
+    it { should have_many :order_items }
+    it { should have_many(:items).through(:order_items) }
+  end
 end
