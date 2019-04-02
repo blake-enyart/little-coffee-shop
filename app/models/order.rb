6a8dd1845e1a042b3a5d3511c8ff1fffd1d3a82a
Matt Levy <milevy1@gmail.com>
2019-04-02 16:05:47 -0600

    Added order.status enums

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,6 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :order_items
   has_many :items, through: :order_items
+
+  enum status: ['pending', 'packaged', 'shipped', 'cancelled']
 end
