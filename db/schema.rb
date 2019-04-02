4547ef27f101360d793bc42a4ea952edb8ffd27d
Matt Levy <milevy1@gmail.com>
2019-04-01 21:14:23 -0600

    Schema updated to reflect all migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190401225925) do
+ActiveRecord::Schema.define(version: 20190402025636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "image_url"
+    t.integer "quantity"
+    t.float "price"
+    t.boolean "enabled", default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.bigint "order_id"
+    t.bigint "item_id"
+    t.float "order_price"
+    t.integer "quantity"
+    t.boolean "fulfilled", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_order_items_on_item_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+  end
 
   create_table "orders", force: :cascade do |t|
     t.integer "status"
@@ -37,5 +62,8 @@ ActiveRecord::Schema.define(version: 20190401225925) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "items", "users"
+  add_foreign_key "order_items", "items"
+  add_foreign_key "order_items", "orders"
   add_foreign_key "orders", "users"
 end
