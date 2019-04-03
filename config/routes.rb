a238e535276cf73db97fcde4bf52d65f60d409cd
Matt Levy <milevy1@gmail.com>
2019-04-02 22:18:48 -0600

    Added Item show page route aka item_path(:id)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "welcome#index"
-  resources :items, only: [:index]
+  resources :items, only: [:index, :show]
   # get "/merchants", to: merchants
   resources :merchants, only: [:index]
   get '/cart', to: "cart#index", as: :cart
