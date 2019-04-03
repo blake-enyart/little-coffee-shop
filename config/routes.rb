664ad2685c3f06aa3fa58599c3ff809797230407
Blake <blake.enyart@gmail.com>
2019-04-03 08:28:35 -0600

    WIP route development

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "welcome#index"
+
   resources :items, only: [:index]
-  # get "/merchants", to: merchants
+
+  #merchant routes
   resources :merchants, only: [:index]
+  get '/dashboard', to: 'merchants#show', as: :dashboard
+
+  #admin routes
+  namespace :admin do
+    get '/dashboard', to: 'dashboard#show', as: :dashboard
+  end
+
+  #user routes
   get '/cart', to: "cart#index", as: :cart
+  get '/profile', to: "users#show", as: :profile
+
+  #all users routes
   get '/login', to: "sessions#new", as: :login
   get '/logout', to: "sessions#destroy", as: :logout
   resources :users, only: [:new]
-
-  get '/profile', to: "users#show", as: :profile
-
-
 end
