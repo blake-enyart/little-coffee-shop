8d02cc4ddcf812f0fe13da99ea46655ea6fc04a1
Matt Levy <milevy1@gmail.com>
2019-04-04 13:07:11 -0600

    Added before_action :set_cart to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :current_user, :current_reguser?, :merchant_user?, :admin_user?
+  before_action :set_cart
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
@@ -16,6 +18,10 @@ class ApplicationController < ActionController::Base
 
   def admin_user?
     current_user && current_user.admin?
+  end
+
+  def set_cart
+    @cart ||= Cart.new(session[:cart])
   end
 
 end
