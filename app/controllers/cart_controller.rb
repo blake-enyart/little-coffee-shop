542d22111ad3c77ac7c15beeebed7c06200c855e
Matt Levy <milevy1@gmail.com>
2019-04-06 13:40:59 -0600

    Added flash message to cart#index controller if @cart.empty?

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -3,6 +3,9 @@ class CartController < ApplicationController
 
   def index
     render file: "/public/404", status: 404 if merchant_user? || admin_user?
+    if @cart.empty?
+      flash[:cart_empty] = "Your cart is empty."
+    end
   end
 
   def create
