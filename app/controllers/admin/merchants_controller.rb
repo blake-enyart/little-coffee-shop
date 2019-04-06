a5fbdead6477b1b84bd31111f1653756e607534a
mikekarnes123 <michaelkarnes.mk@gmail.com>
2019-04-06 15:08:57 -0600

    Finishes user story 60 card 10

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -1,7 +1,13 @@
 class Admin::MerchantsController < ApplicationController
   before_action :error_unless_admin
   def show
-    @user_to_be_updated = User.find(params[:id])
+    @user = User.find(params[:id])
+    case
+    when @user.merchant?
+      @user
+    when @user.user?
+      redirect_to admin_user_path(@user)
+    end
   end
 
   def update
