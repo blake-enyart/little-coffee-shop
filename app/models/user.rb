c668eb1e9623df00abd58ddf5d5a6d4f5feb07cd
Blake <blake.enyart@gmail.com>
2019-04-06 14:55:30 -0600

    Quick addition of end to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,13 +14,14 @@ class User < ApplicationRecord
   has_secure_password allow_blank: true
 
   enum role: ['user', 'merchant', 'admin']
-  
+
   def self.permit_email?(email)
     if self.find_by(email: email)
       return false
     end
     true
-
+  end
+  
   def disable_merchant_items
     items.each {|item| item.disable}
   end
