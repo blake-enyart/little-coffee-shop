5fc34237168e98bfeb9879af26650d2848626628
Matt Levy <milevy1@gmail.com>
2019-04-05 18:59:21 -0600

    Added Cart instance methods #items, #sub_total, #grand_total

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,6 +3,7 @@ class Cart
 
   def initialize(initial_contents)
     @contents = initial_contents || Hash.new(0)
+    @contents.default = 0
   end
 
   def total_count
@@ -15,6 +16,27 @@ class Cart
 
   def count_of(id)
     @contents[id.to_s].to_i
+  end
+
+  def items
+    items_hash = Hash.new(0)
+    @contents.each do |item_id, quantity|
+      item = Item.find(item_id)
+      items_hash[item] = quantity
+    end
+    items_hash
+  end
+
+  def sub_total(item, quantity)
+    item.price * quantity
+  end
+
+  def grand_total
+    total = 0.0
+    items.each do |item, quantity|
+      total += item.price * quantity
+    end
+    total
   end
 
 end
