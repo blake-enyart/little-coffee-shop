980aee78fe06231a84aa000e50f928ec338026fa
mikekarnes123 <michaelkarnes.mk@gmail.com>
2019-04-08 13:59:46 -0600

    Adds user story 41

--- spec/features/merchants/merchants_index_page_spec.rb
+++ spec/features/merchants/merchants_index_page_spec.rb
@@ -29,4 +29,37 @@ RSpec.describe "All users can see a merchants index page", type: :feature do
       expect(page).to_not have_css("#merchant-#{@merchant_3.id}")
     end
   end
+
+  context 'as an admin' do
+    it "shows all merchants including disabled ones with proper links" do
+      admin = create(:admin)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit merchants_path
+
+      within "#merchant-#{@merchant_1.id}" do
+        expect(page).to have_link(@merchant_1.name, href: admin_merchant_path(@merchant_1))
+        expect(page).to have_content(@merchant_1.city)
+        expect(page).to have_content(@merchant_1.state)
+        expect(page).to have_content(@merchant_1.created_at)
+        expect(page).to have_link("Disable")
+      end
+
+      within "#merchant-#{@merchant_2.id}" do
+        expect(page).to have_link(@merchant_2.name, href: admin_merchant_path(@merchant_2))
+        expect(page).to have_content(@merchant_2.city)
+        expect(page).to have_content(@merchant_2.state)
+        expect(page).to have_content(@merchant_2.created_at)
+        expect(page).to have_link("Disable")
+      end
+      
+      within "#merchant-#{@merchant_3.id}" do
+        expect(page).to have_link(@merchant_3.name)
+        expect(page).to have_content(@merchant_3.city)
+        expect(page).to have_content(@merchant_3.state)
+        expect(page).to have_content(@merchant_3.created_at)
+        expect(page).to have_link("Enable")
+      end
+    end
+  end
 end
