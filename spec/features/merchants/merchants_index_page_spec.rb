364fc0b91eb4ab9970793bedd9ee0452cc6a81b2
mikekarnes123 <michaelkarnes.mk@gmail.com>
2019-04-08 16:02:40 -0600

    adds user story 38

--- spec/features/merchants/merchants_index_page_spec.rb
+++ spec/features/merchants/merchants_index_page_spec.rb
@@ -52,7 +52,7 @@ RSpec.describe "All users can see a merchants index page", type: :feature do
         expect(page).to have_content(@merchant_2.created_at)
         expect(page).to have_link("Disable")
       end
-      
+
       within "#merchant-#{@merchant_3.id}" do
         expect(page).to have_link(@merchant_3.name)
         expect(page).to have_content(@merchant_3.city)
@@ -60,6 +60,15 @@ RSpec.describe "All users can see a merchants index page", type: :feature do
         expect(page).to have_content(@merchant_3.created_at)
         expect(page).to have_link("Enable")
       end
+    end
+
+    it 'should take admin to admin merchant show from merch index links' do
+      admin = create(:admin)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit merchants_path
+      click_link(@merchant_1.name)
+      expect(current_path).to eq(admin_merchant_path(@merchant_1))
     end
   end
 end
