eb4d6be7acbbed0a1d4c9d0334625f9b9921efea
mikekarnes123 <michaelkarnes.mk@gmail.com>
2019-04-09 12:45:17 -0600

    Adds block to update that disables merchant if disable: true is in params

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -6,10 +6,18 @@ class Admin::MerchantsController < Admin::BaseController
   end
 
   def update
-    user_to_be_updated = User.find(params[:id])
-    user_to_be_updated.disable_merchant_items
-    user_to_be_updated.update(role: 0)
-    flash[:notice] = "Merchant #{user_to_be_updated.name} downgraded"
-    redirect_to admin_user_path(user_to_be_updated)
+    if params[:disable]
+      merchant = User.find(params[:id])
+      merchant.disable_merchant_items
+      merchant.update(enabled: false)
+      redirect_to merchants_path
+      flash[:merchant_disabled] = "#{merchant.name} is now disabled"
+    else
+      user_to_be_updated = User.find(params[:id])
+      user_to_be_updated.disable_merchant_items
+      user_to_be_updated.update(role: 0)
+      flash[:notice] = "Merchant #{user_to_be_updated.name} downgraded"
+      redirect_to admin_user_path(user_to_be_updated)
+    end
   end
 end
