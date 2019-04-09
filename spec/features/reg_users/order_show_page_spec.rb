d29ae35a56efb4b10e07f6834c75a8bac1bfef9f
Rene Casco <34440390+renecasco@users.noreply.github.com>
2019-04-09 16:31:54 -0600

    adds test setup

---
+++ spec/features/reg_users/order_show_page_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'User Order Show Page' do
+  describe 'when a registered user visits their  Profile Orders page' do
+    scenario 'they click on an order id number' do
+      it 'lets them see that order show page' do
+        @user = create(:user)
+        @order_1 = create(:order, user: @user)
+
+        @item_1 = create(:item)
+        @item_2 = create(:item)
+
+        @order_item_1= create(:fulfilled_order_item, item: @item_1, order: @order_1)
+        @order_item_2= create(:fulfilled_order_item, item: @item_2, order: @order_1)
+
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+    
+      end
+    end
+  end
+end
+
+# As a registered user
+# When I visit my Profile Orders page
+# And I click on a link for order's show page
+# My URL route is now something like "/profile/orders/15"
+# I see all information about the order, including the following information:
+# - the ID of the order
+# - the date the order was made
+# - the date the order was last updated
+# - the current status of the order
+# - each item I ordered, including name, description, thumbnail, quantity, price and subtotal
+# - the total quantity of items in the whole order
+# - the grand total of all items for that order
