b9da7f82e4579d03fa86147753c56d1c5b9d0ddc
Matt Levy <milevy1@gmail.com>
2019-04-08 19:46:04 -0600

    Added spec for Order instance method #grand_total

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -10,4 +10,20 @@ RSpec.describe Order, type: :model do
     it { should have_many :order_items }
     it { should have_many(:items).through(:order_items) }
   end
+
+  describe 'Instance methods' do
+    describe '#grand_total' do
+      it 'returns the grand total $ amount for an order' do
+        item_1 = create(:item)
+        item_2 = create(:item)
+        order = create(:order)
+        order_item_1 = create(:order_item, order: order, item: item_1)
+        order_item_2 = create(:order_item, order: order, item: item_2)
+
+        expected = (order_item_1.order_price * order_item_1.quantity) + (order_item_2.order_price * order_item_2.quantity)
+
+        expect(order.grand_total).to eq(expected)
+      end
+    end
+  end
 end
