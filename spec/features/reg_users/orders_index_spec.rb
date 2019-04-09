60098c927025d722cdc2d46939611e67d815b135
Matt Levy <milevy1@gmail.com>
2019-04-08 20:17:40 -0600

    Updated test for profile_orders_path and profile_order_path

--- spec/features/reg_users/orders_index_spec.rb
+++ spec/features/reg_users/orders_index_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe 'User Order Show Page' do
     end
 
     it 'shows a link on my profile page called My Orders' do
-      expect(page).to have_link("My Orders", href: orders_path)
+      expect(page).to have_link("My Orders", href: profile_orders_path)
     end
 
     scenario 'clicking this link takes me to /profile/orders' do
@@ -39,7 +39,7 @@ RSpec.describe 'User Order Show Page' do
       click_button "Log In"
 
       visit profile_path
-      expect(page).to_not have_link("My Orders", href: orders_path)
+      expect(page).to_not have_link("My Orders", href: profile_orders_path)
     end
   end
 
@@ -56,13 +56,13 @@ RSpec.describe 'User Order Show Page' do
       fill_in "password", with: @user.password
       click_button "Log In"
 
-      visit orders_path
+      visit profile_orders_path
     end
 
     it 'shows order info for all my orders' do
       within "#order-#{@order_1.id}" do
         expect(page).to have_content("Order ID: #{@order_1.id}")
-        expect(page).to have_link(@order_1.id, href: order_path(@order_1))
+        expect(page).to have_link(@order_1.id.to_s, href: profile_order_path(@order_1))
         expect(page).to have_content("Date of Order: #{@order_1.created_at}")
         expect(page).to have_content("Last Updated: #{@order_1.updated_at}")
         expect(page).to have_content("Status: #{@order_1.status}")
@@ -71,7 +71,7 @@ RSpec.describe 'User Order Show Page' do
 
       within "#order-#{@order_2.id}" do
         expect(page).to have_content("Order ID: #{@order_2.id}")
-        expect(page).to have_link(@order_2.id, href: order_path(@order_2))
+        expect(page).to have_link(@order_2.id.to_s, href: profile_order_path(@order_2))
         expect(page).to have_content("Date of Order: #{@order_2.created_at}")
         expect(page).to have_content("Last Updated: #{@order_2.updated_at}")
         expect(page).to have_content("Status: #{@order_2.status}")
