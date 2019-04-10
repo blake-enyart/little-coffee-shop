4703b43e9ff2c33b68f2d6c0c2152cdf920496ae
Matt Levy <milevy1@gmail.com>
2019-04-09 18:37:48 -0600

    Added merchants/items#delete_item route

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get '/dashboard', to: 'merchants#show', as: :dashboard
   get '/dashboard/items', to: 'merchants/items#index', as: :dashboard_items
   get '/enable_item/:id', to: 'merchants/items#enable_item', as: :enable_item
+  get '/delete_item/:id', to: 'merchants/items#delete_item', as: :delete_item
 
   #admin routes
   namespace :admin do
