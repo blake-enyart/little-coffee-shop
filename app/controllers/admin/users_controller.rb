940211679d941b4ff19b21e7dac565801ec7347f
Blake Enyart <38588295+blake-enyart@users.noreply.github.com>
2019-04-10 17:52:12 -0600

    Update users_controller.rb

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,7 @@ class Admin::UsersController < Admin::BaseController
 
   def show
     @user = User.find(params[:id])
-    if @user.role == 'merchant'
+    if @user.merchant?
       redirect_to admin_merchant_path(@user)
     end
   end
