85e11714fa690165b33365399d201fb3d8038f73
Rene Casco <34440390+renecasco@users.noreply.github.com>
2019-04-10 14:22:56 -0600

    initial setup for test

---
+++ spec/features/admins/admin_can_see_all_orders_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe "Admin Dashboard" do
+  describe 'When an admin user logs into his dashbopard' do
+    it 'lets them see all orders in the system' do
+      @order_1 = create(:order)
+      @order_2 = create(:packaged_order)
+      @order_3 = create(:shipped_order)
+      @order_4 = create(:cancelled_order)
+
+      @admin = create(:admin)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      within "#order-#{@order_1.id}" do
+        expect(page).to have_link(@order_1.user, href: admin_user_path(@order_1.user))
+        expect(page).to have_content("Order ID: #{@order_1.id}")
+        expect(page).to have_content("Date of Order: #{@order_1.created_at}")
+      end
+      within "#order-#{@order_2.id}" do
+        expect(page).to have_link(@order_1.user, href: admin_user_path(@order_2.user))
+        expect(page).to have_content("Order ID: #{@order_2.id}")
+        expect(page).to have_content("Date of Order: #{@order_2.created_at}")
+      end
+      within "#order-#{@order_3.id}" do
+        expect(page).to have_link(@order_1.user, href: admin_user_path(@order_3.user))
+        expect(page).to have_content("Order ID: #{@order_3.id}")
+        expect(page).to have_content("Date of Order: #{@order_3.created_at}")
+      end
+      within "#order-#{@order_4.id}" do
+        expect(page).to have_link(@order_1.user, href: admin_user_path(@order_4.user))
+        expect(page).to have_content("Order ID: #{@order_4.id}")
+        expect(page).to have_content("Date of Order: #{@order_4.created_at}")
+      end
+
+    end
+  end
+end
+
+# Orders are sorted by "status" in this order:
+#
+# - packaged
+# - pending
+# - shipped
+# - cancelled
