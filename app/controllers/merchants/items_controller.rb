6f2cb8da2afc19cf919963524eba90b4d6f22de4
Matt Levy <milevy1@gmail.com>
2019-04-09 18:39:43 -0600

    Added #delete_item controller action to the merchants/items_controller

--- app/controllers/merchants/items_controller.rb
+++ app/controllers/merchants/items_controller.rb
@@ -13,4 +13,13 @@ class Merchants::ItemsController < ApplicationController
     redirect_to dashboard_items_path
   end
 
+  def delete_item
+    item = Item.find(params[:id])
+    item.destroy
+
+    flash[:item_delete_success] = "#{item.name} has been deleted."
+
+    redirect_to dashboard_items_path
+  end
+
 end
