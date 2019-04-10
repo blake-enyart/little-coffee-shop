0dd9c0e15fd5fd3126a43b0d212872f80caa7156
mikekarnes123 <michaelkarnes.mk@gmail.com>
2019-04-10 13:46:04 -0600

    Adds login to admin user show spec

--- spec/features/admins/admin_user_show_spec.rb
+++ spec/features/admins/admin_user_show_spec.rb
@@ -20,15 +20,28 @@ RSpec.describe 'Admin User Show Page', type: :feature do
       scenario 'admin clicks upgrade button' do
         admin = create(:admin)
         user_1 = create(:user)
-        allow_any_instance_of(ApplicationController).to \
-        receive(:current_user).and_return(admin)
-
+        visit login_path
+        fill_in "email", with: admin.email
+        fill_in "password", with: admin.password
+        click_button "Log In"
         visit admin_user_path(user_1)
         click_link 'Upgrade'
+
         expect(current_path).to eq(admin_merchant_path(user_1))
+
         user_1.reload
+
         expect(page).to have_content("#{user_1.name} has been upgraded")
         expect(user_1.role).to eq("merchant")
+
+        click_on 'Logout'
+        visit login_path
+        fill_in "email", with: user_1.email
+        fill_in "password", with: user_1.password
+        click_button "Log In"
+        visit dashboard_path
+
+        expect(page).to have_http_status(200)
       end
     end
   end
