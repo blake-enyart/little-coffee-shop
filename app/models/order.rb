566a0b81fe3b820cd509084ff6bc842ff248c921
Matt Levy <milevy1@gmail.com>
2019-04-11 07:12:10 -0600

    Added missing end, likely from merge conflict

--- app/models/order.rb
+++ app/models/order.rb
@@ -17,6 +17,7 @@ class Order < ApplicationRecord
 
   def self.sort_by_status
     Order.order("status=3, status=2, status=0, status=1")
+  end
 
   def pending_to_packaged
     if self.order_items.pluck(:fulfilled).all?
