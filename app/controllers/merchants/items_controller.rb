35d6b3b1659d8d92f0d2006b436dfaffb6afd133
Noah Flint <noah.flint24@gmail.com>
2019-04-10 18:16:36 -0600

    refactored merchants items controller

--- app/controllers/merchants/items_controller.rb
+++ app/controllers/merchants/items_controller.rb
@@ -9,22 +9,6 @@ class Merchants::ItemsController < ApplicationController
     @item = Item.new
   end
 
-  def create
-    require 'pry'; binding.pry
-    @item = current_user.items.new(item_params)
-    if @item.image_url == ""
-      @item.image_url = "https://i.pinimg.com/originals/2a/84/90/2a849069c7f487f71bb6594dffb84e5e.png"
-    end
-    if @item.save
-      flash[:success] = "#{@item.name} is now saved and available for sale."
-      redirect_to dashboard_items_path
-    else
-      flash[:error] = @item.errors.full_messages.join(", ")
-      # redirect_to new_merchant_item_path
-      render :new
-    end
-  end
-
   def enable_item
     item = Item.find(params[:id])
     item.update(enabled: true)
@@ -33,11 +17,4 @@ class Merchants::ItemsController < ApplicationController
 
     redirect_to dashboard_items_path
   end
-
-  private
-
-  def item_params
-    params.require(:item).permit(:name, :description, :image_url, :quantity, :price)
-  end
-
 end
